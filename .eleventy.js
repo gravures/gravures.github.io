8b927decf4365f3e6e861bbf84b3d6713f7d910b
Gilles Coissac <gilles@ideographe.fr>
2024-11-01 15:50:41 +0100

    feat: add the favicon plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,18 @@
+import faviconsPlugin from "eleventy-plugin-gen-favicons";
 import SassHandler from "./src/_11ty/handlers/SassHandler.js";
 import include_section from "./src/_11ty/shortcodes/include_section.js";
 import html_prettify from "./src/_11ty/transforms/html_prettify.js";
 
 export default function (eleventyConfig) {
     eleventyConfig.ignores.add("**/_11ty/**");
+
+    // plugins
+    eleventyConfig.addPlugin(faviconsPlugin, {});
+
     // scss handlers
     eleventyConfig.addTemplateFormats("scss");
     eleventyConfig.addExtension("scss", SassHandler);
+
     // shortCodes
     eleventyConfig.addNunjucksShortcode("include_section", include_section);
 
