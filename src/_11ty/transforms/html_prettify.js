85e006d2aa96cc47d8687b9d26d255b62c50e1ba
Gilles Coissac <gilles@ideographe.fr>
2024-11-04 11:34:28 +0100

    fix: prevent html-prettify touching non *.html file

--- src/_11ty/transforms/html_prettify.js
+++ src/_11ty/transforms/html_prettify.js
@@ -1,5 +1,9 @@
 import prettify from "html-prettify";
 
 export default function (content) {
-    return prettify(content, { char: " ", count: 4 });
+    if ((this.page.outputPath || "").endsWith(".html")) {
+        return prettify(content, { char: " ", count: 4 });
+    }
+    // If not an HTML output, return content as-is
+    return content;
 };
