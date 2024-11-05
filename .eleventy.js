70a8713881434403b9856d8e3c0fb12c702d93a8
Gilles Coissac <gilles@ideographe.fr>
2024-11-05 11:15:08 +0100

    feat: Use i18 eleventy plugin and refactor src files

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,24 @@
+import { EleventyI18nPlugin } from "@11ty/eleventy";
 import faviconsPlugin from "eleventy-plugin-gen-favicons";
 import SassHandler from "./src/_11ty/handlers/SassHandler.js";
 import include_partial from "./src/_11ty/shortcodes/include_partial.js";
 import html_prettify from "./src/_11ty/transforms/html_prettify.js";
+
 
 export default function (eleventyConfig) {
     // ignore directories
     eleventyConfig.ignores.add("**/_11ty/**");
 
     // globals
-    eleventyConfig.addGlobalData("layouts.root", "layouts");
-    eleventyConfig.addGlobalData("layouts.partials", "partial");
+    eleventyConfig.addGlobalData("layout", "layouts/site.njk");
+    eleventyConfig.addGlobalData("type", "default");
 
     // plugins
     eleventyConfig.addPlugin(faviconsPlugin, {});
+    eleventyConfig.addPlugin(EleventyI18nPlugin, {
+        defaultLanguage: "fr",
+        errorMode: "allow-fallback"
+    })
 
     // scss handlers
     eleventyConfig.addTemplateFormats("scss");
