45b41c17a6d1563369f0180468434ea9fca7da98
Gilles Coissac <gilles@ideographe.fr>
2024-12-04 12:39:39 +0100

    feat(_11ty): update eleventy.js
    -left out config frrom default function
    -add the render plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,19 @@
-import { EleventyI18nPlugin } from "@11ty/eleventy";
-import faviconsPlugin from "eleventy-plugin-gen-favicons";
 import { readFileSync } from "fs";
 import path from "path";
+
+import { EleventyI18nPlugin, EleventyRenderPlugin } from "@11ty/eleventy";
+import pluginWebc from "@11ty/eleventy-plugin-webc";
+import faviconsPlugin from "eleventy-plugin-gen-favicons";
+
 import SassHandler from "./src/_11ty/handlers/SassHandler.js";
-import include_partial from "./src/_11ty/shortcodes/include_partial.js";
+import partial from "./src/_11ty/shortcodes/partial.js";
 import html_prettify from "./src/_11ty/transforms/html_prettify.js";
 
+
+
+/** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default function (eleventyConfig) {
+    // ignores
     eleventyConfig.ignores.add("./src/_assets/js/**/*");
 
     // globals
@@ -14,48 +21,54 @@ export default function (eleventyConfig) {
     eleventyConfig.addGlobalData("type", "default");
 
     // plugins
-    eleventyConfig.addPlugin(faviconsPlugin, {});
     eleventyConfig.addPlugin(EleventyI18nPlugin, {
         defaultLanguage: "fr",
         errorMode: "allow-fallback"
-    })
+    });
+    eleventyConfig.addPlugin(EleventyRenderPlugin);
+    eleventyConfig.addPlugin(pluginWebc, {
+        components: "./src/_includes/components/**/*.webc",
+    });
+    eleventyConfig.addPlugin(faviconsPlugin, {});
+
+    // shortCodes
+    eleventyConfig.addPlugin(partial, {});
 
     // scss handlers
     eleventyConfig.addTemplateFormats("scss");
     eleventyConfig.addExtension("scss", SassHandler);
 
-    // shortCodes
-    eleventyConfig.addPlugin(include_partial, {});
-
     // transforms
     eleventyConfig.addTransform("htmlprettify", html_prettify);
-
-    // bundles
-    eleventyConfig.addBundle("javascript", {
-        toFileDirectory: "js",
-        outputFileExtension: "js",
-    });
-
-    eleventyConfig.addBundle("css", {
-        toFileDirectory: "css",
-        outputFileExtension: "css",
-    });
 
     // node.js libraries
     let pkg = JSON.parse(readFileSync(path.resolve("./package.json")));
     for (let bundle in pkg.bundleDependencies) {
-        let dir = path.join("node_modules", pkg.bundleDependencies[bundle])
+        let dir = path.join("node_modules", pkg.bundleDependencies[bundle]);
         eleventyConfig.addPassthroughCopy(dir);
         console.log(dir);
     }
+};
 
-    // 11ty configuration
-    return {
-        dir: {
-            input: "src",
-            output: "_site",
-        },
-        markdownTemplateEngine: "njk",
-        htmlTemplateEngine: "njk",
-    };
+
+export const config = {
+    // Control which files Eleventy will process
+    templateFormats: [
+        "md",
+        "njk",
+        "html",
+        "liquid",
+        "11ty.js",
+    ],
+
+    // Pre-process *.md files with
+    markdownTemplateEngine: "njk",
+
+    // Pre-process *.html files with:
+    htmlTemplateEngine: "njk",
+
+    dir: {
+        input: "src",
+        output: "_site",
+    },
 };
