750be2db293a5338777116cd2f426bf9b9c74522
Gilles Coissac <gilles@ideographe.fr>
2024-12-05 16:43:02 +0100

    refactor(_11ty): makes html prettfy a plugin

--- src/_11ty/transforms/html_prettify.js
+++
@@ -1,9 +0,0 @@
-import prettify from "html-prettify";
-
-export default function (content) {
-    if ((this.page.outputPath || "").endsWith(".html")) {
-        return prettify(content, { char: " ", count: 4 });
-    }
-    // If not an HTML output, return content as-is
-    return content;
-};
